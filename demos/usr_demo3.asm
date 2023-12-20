99d9a30f0b717db79820694070037f00bf10550e
Aleksey Mandrykin <almandrykin@ya.ru>
2023-12-21 02:47:40 +0300

    Add files via upload
    Печатает коды нажатых клавиш

---
+++ demos/usr_demo3.asm
@@ -0,0 +1,40 @@
+; Печатает коды нажатых клавиш на отладочной консоле
+
+; --- Точка входа при отладке на "голом железе". Она должна быть первой в файле
+function _test_string
+	load	r14, 0x8000 ; set stack
+	notch
+	jmp	entry
+entry:
+
+function   user_main
+	push	r15
+	lea	r1, $hi_str	
+	call	_puts		
+loop:
+	call	_getchar
+	push	r0
+	lea	r1, $dec_str	
+	call	_puts		
+	mov	r0, (r14)
+	call	_print_dec
+	lea	r1, $hex_str	
+	call	_puts		
+	mov	r0, (r14)
+	call	_print_hex
+	call	_newline
+	pop	r0
+	load	r1, 0x51
+	cmp	r1, r0
+	jne	loop
+	pop	r15
+	return
+end
+
+include		../lib/tty/tty.asm
+include		../lib/emulate/div.asm
+include		../lib/tty/print_dec.asm
+
+$hi_str		db	'Press Q to quit',13,10,0
+$dec_str	db	'Decimal ',0
+$hex_str	db	' Hex ',0
