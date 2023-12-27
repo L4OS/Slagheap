a3f79912da453f50e3372b3bedff0d3307aa7fb6
Aleksey Mandrykin <alman@fast-report.com>
2023-12-27 03:20:35 +0300

    reorder_dir_structure

---
+++ lib/asm/console/uptime.asm
@@ -0,0 +1,148 @@
+;
+; Show device uptime
+; 
+; --- Точка входа при отладке. Она должна быть первой в файле
+; Не должна вызываться программой и служит как тестовый заголовок
+function test_uptime
+		load	r14, 0x2000 ; set stack
+		notch
+		jmp	entry
+entry:
+		call	_show_uptime  	
+		lea	r1, $uptime_string
+		call	_puts
+		call	_newline
+		send
+end
+
+; Переделать на $lib_string@uptime
+
+$uptime_string	db	'TEST UPT',0
+
+
+; --- Поля Function Control Block ------------------------------
+
+;$return_address		equ		0	; Позиция адреса возврата
+$seconds_count_lo	equ		1	; Количество секунд с момента старта устройства
+$seconds		equ		2	; Количество секунд 
+$minutes		equ		3	; Количество минут
+$hours			equ		4	; Количество часов
+
+; ------ Именование регистров ------
+assign		r3	TaskState		; Состояние задачи
+assign		r2	TaskPtr			; Указатель на задачу
+assign		r1	TimeoutValue		; Количество тактов до следующей операции планирования
+assign		r0	TCB			; Указатель на область данных задачи
+
+
+function   _show_uptime
+	dec	r14, 12		; Освобождение места на стеке для локальных переменных
+	dec	r14, 12		; Освобождение места на стеке для локальных переменных
+	mov	(r14), r15	; Адрес возврата из функции
+
+	lea	r1, $tick_str
+	call	_puts
+
+	call	_get_sysclock
+	
+	push	r0
+	mov	r0, r1
+	call	_print_hex
+	pop	r0
+	call	_print_hex
+
+	call	_get_sysclock		; ну и что
+
+;	mov	r2, 100000000
+;	mov	r2, 50000000	; 50 MHz version
+	mov	r2, 1000	; DWORD GetTickCount();
+	call	_div64
+
+	mov	r14[$seconds_count_lo], r0
+
+	lea	r1, $is_str
+	call	_puts
+
+	mov	r0,  r14[$seconds_count_lo]
+	call	_print_dec
+
+	lea	r1, $sec_mid_str
+	call	_puts
+
+	mov	r0,  r14[$seconds_count_lo]
+	xor	r1, r1
+	mov	r2, 60
+	call	_div64
+	mov	r14[$seconds], R2
+
+	xor	r1, r1
+	mov	r2, 60
+	call	_div64
+	mov	r14[$minutes], R2
+
+	xor	r1, r1
+	mov	r2, 24
+	call	_div64
+	mov	r14[$hours], R2
+
+	or	r0, r0
+	je	skip_days
+	call	_print_dec
+	lea	r1, $days_str
+	call	_puts
+skip_days:
+	movt	r0, r14[$hours]
+	je	skip_hours
+	call	_print_dec
+	lea	r1, $hours_str
+	call	_puts
+skip_hours:
+	movt	r0, r14[$minutes]
+	je	skip_minutes
+	call	_print_dec
+	lea	r1, $min_str
+	call	_puts
+skip_minutes:
+	mov	r0, r14[$seconds]
+	call	_print_dec
+	lea	r1, $sec_fin_str
+	call	_puts
+
+	call	_get_step_counter
+	call	_print_dec
+	lea	r1, $step_count
+	call	_puts
+
+	call	_get_speed_counter
+	call	_print_dec
+	lea	r1, $step_count
+	call	_puts
+
+	mov	r15, (r14)
+	inc	r14, 12
+	inc	r14, 12
+	return
+
+; ------------------------------------------------------------------
+
+end ; of function
+
+$tick_str	db	'Tick ',0
+$is_str		db	' is ',0
+$days_str	db	' days ',0
+$hours_str	db	' hours ',0
+$min_str	db	' min ',0
+$sec_mid_str	db	' seconds = ',0
+$sec_fin_str	db	' sec',13,10,0
+$step_count	db	' total instructions executed',13,10,0
+$speed_count	db	' cpu speed',13,10,0
+
+disable
+else
+ include		../tty/tty.asm
+ include		../emulate/div.asm
+ include		../emulate/sysclock.asm
+ include		../emulate/step_counter.asm
+ include		../emulate/cpu_speed.asm
+ include		../tty/print_dec.asm
+done
---
+++ test_vga.asm
@@ -0,0 +1,233 @@
+function	init_vga
+	load	r14, 0x8000 ; set stack
+	notch
+	jmp	entry
+entry:
+end
+
+function	_test_vga
+enter:
+	push	r15
+	
+	lock
+	set_mr	mr32, r1	; Тест регистров сообщений
+	get_mr  mr32, r7
+	unlock
+
+	; Выбор шрифта по умолчанию
+	lea	r7, $dosfont1
+	call	_select_font
+
+help:
+	lea	r1, $line1	; Адрес строки приветсвия в R1
+	call	_puts		; Вывод данных в UART
+;debug
+	lea	r0, $line1	; Адрес строки приветствия в R1
+	load	r1, 0x00100000	; Позиция на экране - старшие 16 бит номер строки, младшие - номер столбца
+	load	r2, 0xFFFFFFFF  ; Цвет пикселей
+	load	r3, 0x00000000  ; Цвет фона
+	call 	_draw_string
+
+	lea	r0, $line2	; Адрес строки приветствия в R1
+	load	r1, 0x00110000	; Позиция на экране - старшие 16 бит номер строки, младшие - номер столбца
+	load	r2, 0xFFFFFFFF  ; Цвет пикселей
+	load	r3, 0x00000000  ; Цвет фона
+	call 	_vga_puts
+;	call 	_draw_string
+loop:
+	load	r3, 0xfffeffe0	; Обновлене экрана пока не вынесено в функцию
+	mov	(r3), r0	; Обновить экран перед чтением клавиатуры
+
+	load	r0, 1000 	; Ждать событие 1000 мс
+	call	_get_event	; Ввод символа с графического окна
+	or	r0, r0
+	je	timeout
+	load	r6, 0x102	; Ввод символа
+	cmp	r0, r6
+	jne	l1
+    ;   ---- обработка события клавиатуры
+	load	r1, 0xfffeffe8	; WINDOW_WPARAM порт. После прихода события в нём хранится W-param события
+	mov	r0, (r1)	; Чтение с порта хранящего код нажатой клавиши
+	call	_input_char_callback
+	jmp	loop
+l1:
+	load	r6, 0x202	; Отпустили клавишу мыши
+	cmp	r0, r6
+	jne	loop		; Игнорируем остальные события
+    ;   ---- печать позиции отпускания клавиши мыши
+
+	lea	r1, $line1	; Адрес строки приветсвия в R1
+	call	_puts		; Вывод данных в UART
+	load	r0, 0xfffeffe8	; WINDOW_WPARAM порт. После прихода события в нём хранится W-param события
+	mov	r0, (r1)	; Чтение с порта хранящего код нажатой клавиши
+	call	_print_hex
+	load	r0, 0xfffeffec	; WINDOW_WPARAM порт. После прихода события в нём хранится W-param события
+	mov	r0, (r0)	; Чтение с порта хранящего код нажатой клавиши
+	call	_print_hex
+
+	jmp	loop
+timeout:
+	jmp	loop
+exit:
+	pop	r15
+	return			; Возврат из функции
+end
+
+$MOUSE_TEXT1	db	'Mouse button W param: ',0
+$MOUSE_TEXT2	db	'Mouse button W param: ',0
+
+; Вход: R0 - код нажатой клавиши
+function _input_char_callback
+	push	r15
+	cmp	r0, 0x30
+	je	exit
+;	cmp	r0, 0x3f	; Клавиша Вопрос
+;	je	help
+
+	cmp	r0, 0x31	; Клавиша 1
+;	notch
+;	je	_clear_vga_screen  ; Надо дорабатывать компилятор ассемблера чтобы оптимизировать
+ 	jne	lab1
+	call	_clear_vga_screen
+	jmp	exit
+lab1:
+lab2:
+	cmp	r0, 0x32
+	jne	lab3
+	lea	r7, $dosfont1
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab3:
+	cmp	r0, 0x33
+	jne	lab4
+	lea	r7, $dosfont2
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab4:
+	cmp	r0, 0x34
+	jne	lab5
+	lea	r7, $dosfont3
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab5:
+	cmp	r0, 0x35
+	jne	lab6
+	lea	r7, $dosfont4
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab6:
+	cmp	r0, 0x36
+	jne	lab7
+	lea	r7, $dosfont5
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab7:
+	cmp	r0, 0x37
+	jne	lab8
+	lea	r7, $dosfont6
+	call	_select_font
+	call	_test_chars
+	jmp	exit
+lab8:
+	cmp	r0, 0x38 ; key 8
+	jne	lab9
+	load 	r0, 16
+	call	_shift_screen_vertically
+	jmp	exit
+lab9:
+
+	cmp	r0, 0x39 ; 'key 9'
+	jne	lab10
+	load 	r0, -16
+	call	_shift_screen_vertically
+	jmp	exit
+lab10:
+	cmp	r0, 0x5b ; key [
+	jne	lab11
+	load 	r0, 8
+	call	_shift_screen_horizontally
+	jmp	exit
+lab11:
+
+	cmp	r0, 0x5d ; 'key ['
+	jne	lab12
+	load 	r0, -8
+	call	_shift_screen_horizontally
+	jmp	exit
+lab12:
+
+exit:	pop	r15
+	return
+end
+
+$line1 	db	'1 - clear vga screen  ',13,10  ,0 
+$line2	db	'2 - CYRTHIN-Nesterenko-8x16   ',13,10,
+$line3	db	'3 - DK-Feoktistov-8x16',13,10
+	db	'4 - beta-Chi-Sovt-8x16',13,10
+	db	'5 - EDFN-Anry-VGA3-8x16   ',13,10
+	db	'6 - MYFONT-8x16   ',13,10
+	db	'7 - Goryachev-UNI_8X16',13,10,
+	db	'8 - scroll screen down',13,10,
+	db	'9 - scroll screen up',13,10,
+	db	'0 - exit to prev menu',13,10,0
+
+; VGA 640x480x32
+include		lib/vga/clear_screen.asm
+include		lib/vga/draw_char.asm
+include		lib/vga/draw_string.asm
+include		lib/vga/get_event.asm
+include		lib/vga/scroll.asm
+
+include		lib/tty/tty.asm
+include		lib/emulate/div.asm
+
+$dosfont1		import		lib/vga/fonts/CYRTHIN-Nesterenko-8x16.fnt 
+$dosfont2		import		lib/vga/fonts/DK-Feoktistov-8x16.fnt 
+$dosfont3		import		lib/vga/fonts/beta-Chi-Sovt-8x16.fnt   
+$dosfont4		import		lib/vga/fonts/EDFN-Anry-VGA3-8x16.FNT  
+$dosfont5		import		lib/vga/fonts/MYFONT-8x16.FNT   
+$dosfont6		import		lib/vga/fonts/Goryachev-UNI_8X16.fnt    
+
+function _test_chars
+	push	r15
+	load 	r8, 0
+	load 	r9, 256
+	load 	r1, 0x80000000
+	mov	r13, r1
+	load	r11, 32
+loop:
+	mov 	r1, r13		; Начало строки
+next_pos:
+	mov	r0, r8
+	load	r2, 0xffffffff
+	load	r3, 0x00000000
+	mov	r12, r1
+	call	_draw_char
+	dec	r11
+	je	newline
+	mov	r1, r12
+	inc	r1, 16
+	inc	r1, 16
+	inc	r8
+	dec	r9
+	jz	exit
+	jmp	next_pos
+newline:
+	load	r11, 40960 ; 640 * 4 * 16
+	clc
+	addc	r13, r11
+	load	r11, 32	
+next:	
+	inc	r8
+	dec	r9
+	jnz	loop	
+exit:
+	pop	r15
+	return
+end
+
